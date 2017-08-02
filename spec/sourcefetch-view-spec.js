d5172eb89b5164f2ee4eb6c74898f4abf21c3c51
caroline <carokun@sas.upenn.edu>
2017-08-01 21:26:18 -0700

    dfjioskdjf

---
+++ spec/sourcefetch-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SourcetoggleView from '../lib/sourcetoggle-view';
+
+describe('SourcetoggleView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
